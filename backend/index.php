c4dad5c7dba38e23ef022a1a4015cf4bb40d3a53
Scott Schapker <csschapker94@gmail.com>
2015-11-18 13:49:24 -0500

    Adds route for base of the backend.

--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,11 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
+
+$app->get('/', function() {
+    echo "Welcome to the PokemonDB backend!";
+});
+
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
 });
