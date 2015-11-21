476038b59f6195a937b253cbe4e1e4ef552ef216
EPICmynamesBG <mynamesbg@gmail.com>
2015-11-21 14:29:22 -0500

    more logging

--- backend/index.php
+++ backend/index.php
@@ -34,48 +34,53 @@ $app->configureMode('development', function () use ($app) {
     ));
 });
 
-$app->log->debug("TEST");
-
-
 $app->get('/', function() {
+    $app->log->debug("/");
     echo "Welcome to the PokemonDB backend!";
 });
 
 $app->get('/hello/:name', function ($name) {
+     $app->log->debug("/hello/:".$name);
     echo "Hello, $name";
 });
 
 $app->get("/pokemon", function() use ($app, $db) {
+     $app->log->debug("/pokemon");
     $result = $db->query(SQL::allPokemon());
     $pokemon = Pokemon::getAll($result);
     echo json_encode($pokemon);
 });
 
 $app->get("/pokemon/:id", function($id) use ($app, $db) {
+    $app->log->debug("/pokemon/:".$id);
     $result = $db->query(SQL::pokemonById($id));
     $pokemon = Pokemon::getById($result);
     echo (json_encode($pokemon));
 });
 
 $app->get('/trainers', function() use ($app, $db) {
+    $app->log->debug("/trainers");
     $result = $db->query(SQL::allTrainers());
     $trainers = Trainer::getAll($result);
     echo json_encode($trainers);
 });
 
 $app->get('/trainers/:id', function($id) use ($app, $db) {
+    $app->log->debug("/trainers/:".$id);
     $result = $db->query(SQL::trainerById($id));
     $trainer = Trainer::getById($result);
     echo json_encode($trainer);
 });
 
 $app->get('/types', function() use ($app, $db) {
+    $app->log->debug("/types");
     $result = $db->query(SQL::allTypes());
     $types = Type::getAll($result);
     echo json_encode($types);
 });
 
 $app->get('/types/:id', function($id) use ($app, $db) {
+    $app->log->debug("/types/:".$id);
     $result = $db->query(SQL::typeById($id));
     //echo json_encode($result);
     $type = Type::getById($result);
@@ -83,12 +88,14 @@ $app->get('/types/:id', function($id) use ($app, $db) {
 });
 
 $app->get('/badges', function() use ($app, $db) {
+    $app->log->debug("/badges");
     $result = $db->query(SQL::allBadges());
     $badges = Badge::getAll($result);
     echo json_encode($badges);
 });
 
 $app->get('/badges/:id', function($id) use ($app, $db) {
+    $app->log->debug("/badges/:".$id);
     $result = $db->query(SQL::badgeById($id));
     $badge = Badge::getById($result);
     echo json_encode($badge);
