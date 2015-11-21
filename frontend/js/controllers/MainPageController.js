363c373b03a47ce3822777c300e48d450fa3674c
EPICmynamesBG <mynamesbg@gmail.com>
2015-11-21 01:19:36 -0500

    Completed base design. Working on main.
    Added new banner and font.

--- frontend/js/controllers/MainPageController.js
+++ frontend/js/controllers/MainPageController.js
@@ -1,4 +1,20 @@
 function MainPageController($http, $scope, $location) {
+    $scope.pokemon = [];
 
+    var random = Math.floor(Math.random() * 151).toString();
+    while (random.length < 3) {
+        random = "0" + random;
+    }
+
+    $http({
+        method: 'GET',
+        url: 'http://localhost:8888/backend/pokemon/' + random
+    }).then(function successCallback(response) {
+        console.log($scope.pokemon.data);
+        $scope.pokemon = response.data;
+    }, function errorCallback(response) {
+        alert("Database unreachable. Check console for more info.");
+        console.log(response);
+    });
 
 };
