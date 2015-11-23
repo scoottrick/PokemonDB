7bfebf6c0544bd11d1e661a24ba3d0bf9a0880d4
Scott Schapker <csschapker94@gmail.com>
2015-11-23 16:28:45 -0500

    I did a lot... Sometimes objects that are returned from requests container other objects. There's a singleton for the db connection. Serialize methods are public.

--- backend/index.php
+++ backend/index.php
@@ -10,95 +10,56 @@ require 'model/Badge.php';
 
 error_reporting(E_ALL);
 
-$db = Connection::createConnection();
-
 $app = new \Slim\Slim();
 
-$app->get("/pokemon", function() use ($app, $db) {
-    $result = $db->query(SQL::allPokemon());
-    $pokemon = Pokemon::getAll($result);
-    echo json_encode($pokemon);
+$app->get("/pokemon", function() use ($app) {
+    echo json_encode(Pokemon::getAll());
 });
 
-$app->get("/pokemon/:id", function($id) use ($app, $db) {
-    $result = $db->query(SQL::pokemonById($id));
-    $pokemon = Pokemon::getById($result);
-    echo (json_encode($pokemon));
+$app->get("/pokemon/:id", function($id) use ($app) {
+    $pokemon = Pokemon::getById($id);
+    echo json_encode($pokemon->serialize());
 });
 
-$app->get("/pokemon/:id/owners", function($pokemonId) use ($app, $db) {
-    $result = $db->query(SQL::trainersOwningPokemon($pokemonId));
-    if (mysqli_num_rows($result) > 0) {
-        $trainers = array();
-        foreach ($result as $row) {
-            $trainerId = $row["trainer_id"];
-            $trainerResult = $db->query(SQL::trainerById($trainerId));
-            array_push($trainers, Trainer::getById($trainerResult));
-        }
-        echo json_encode($trainers);
-    }
+$app->get("/pokemon/:id/owners", function($pokemonId) use ($app) {
+    $pokemon = Pokemon::getById($pokemonId);
+    echo json_encode($pokemon->getTrainers());
 });
 
-$app->get("/trainers/:id/owned_pokemon", function($trainerId) use ($app, $db) {
-    $result = $db->query(SQL::pokemonOwnedByTrainer($trainerId));
-    if (mysqli_num_rows($result) > 0) {
-        $pokemon = array();
-        foreach ($result as $row) {
-            $pokemonId = $row["pokemon_id"];
-            $pokemonResult = $db->query(SQL::pokemonById($pokemonId));
-            array_push($pokemon, Pokemon::getById($pokemonResult));
-        }
-        echo json_encode($pokemon);
-    }
+$app->get('/trainers', function() use ($app) {
+    echo json_encode(Trainer::getAll());
 });
 
-$app->get('/trainers', function() use ($app, $db) {
-    $result = $db->query(SQL::allTrainers());
-    $trainers = Trainer::getAll($result);
-    echo json_encode($trainers);
+$app->get('/trainers/:id', function($id) use ($app) {
+    $trainer = Trainer::getById($id);
+    echo json_encode($trainer->serialize());
 });
 
-$app->get('/trainers/:id', function($id) use ($app, $db) {
-    $result = $db->query(SQL::trainerById($id));
-    $trainer = Trainer::getById($result);
-    echo json_encode($trainer);
+$app->get('/gyms', function() use ($app) {
+    echo json_encode(Gym::getAll());
 });
 
-$app->get('/gyms', function() use ($app, $db) {
-    $result = $db->query(SQL::allGyms());
-    $gyms = Gym::getAll($result);
-    echo json_encode($gyms);
+$app->get('/gyms/:id', function($id) use ($app) {
+    $gym = Gym::getById($id);
+    echo json_encode($gym->serialize());
 });
 
-$app->get('/gyms/:id', function($id) use ($app, $db) {
-    $result = $db->query(SQL::gymByName($id));
-    $gym = Gym::getById($result);
-    echo json_encode($gym);
+$app->get('/types', function() use ($app) {
+    echo json_encode(Type::getAll());
 });
 
-$app->get('/types', function() use ($app, $db) {
-    $result = $db->query(SQL::allTypes());
-    $types = Type::getAll($result);
-    echo json_encode($types);
+$app->get('/types/:id', function($id) use ($app) {
+    $type = Type::getById($id);
+    echo json_encode($type->serialize());
 });
 
-$app->get('/types/:id', function($id) use ($app, $db) {
-    $result = $db->query(SQL::typeById($id));
-    echo json_encode($result);
-    $type = Type::getById($result);
-    echo json_encode($type);
+$app->get('/badges', function() use ($app) {
+    echo json_encode(Badge::getAll());
 });
 
-$app->get('/badges', function() use ($app, $db) {
-    $result = $db->query(SQL::allBadges());
-    $badges = Badge::getAll($result);
-    echo json_encode($badges);
-});
-
-$app->get('/badges/:id', function($id) use ($app, $db) {
-    $result = $db->query(SQL::badgeById($id));
-    $badge = Badge::getById($result);
-    echo json_encode($badge);
+$app->get('/badges/:id', function($id) use ($app) {
+    $badge = Badge::getById($id);
+    echo json_encode($badge->serialize());
 });
 
 $app->run();
--- backend/model/Type.php
+++ backend/model/Type.php
@@ -5,19 +5,22 @@ class Type {
 
     public function __construct($data) {
         if (is_array($data)) {
-            $this->id = intval(['type_id']);
+            $this->id = intval($data['type_id']);
             $this->name = $data['type_name'];
         }
     }
 
-    private function serialize() {
+    public function serialize() {
         return array(
             'id' => $this->id,
             'name' => $this->name
         );
     }
 
-    public static function getAll($result) {
+    public static function getAll() {
+        $db = Connection::sharedDB();
+        $result = $db->query(SQL::allTypes());
+
         $types = array();
         if (mysqli_num_rows($result) > 0) {
             foreach ($result as $row) {
@@ -28,11 +31,14 @@ class Type {
         return $types;
     }
 
-    public static function getById($result) {
+    public static function getById($id) {
+        $db = Connection::sharedDB();
+        $result = $db->query(SQL::typeById($id));
+
         if (mysqli_num_rows($result) > 0) {
             $row = $result->fetch_array();
             $type = new Type($row);
-            return $type->serialize();
+            return $type;
         } else {
             return false;
         }
